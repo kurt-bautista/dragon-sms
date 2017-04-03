2e656832dae103928bec8255f60f8a3b6147312a
Kurt Vincent Bautista <kurt.v@outlook.com>
2017-04-03 21:46:43 +0800

    Create format methods in SMS class

--- src/com/bautista/kurt/SMS.java
+++ src/com/bautista/kurt/SMS.java
@@ -3,8 +3,11 @@ package com.bautista.kurt;
 import framework.CheckFormat;
 import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
 import io.github.lukehutch.fastclasspathscanner.scanner.ScanResult;
-import room.RoomCommandManager;
+import room.*;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -23,21 +26,52 @@ public class SMS {
         this.rcm = new RoomCommandManager();
     }
 
-    public String send(@CheckFormat(regexp = "(?i)^\\s*\\w+\\s*\\w*\\s*$") String message) {
-        String[] params = format(message);
-        rcm.processRoom(session.getCurrentRoom(), session.getGameState(), params[0]);
+    public String send(@CheckFormat(regexp = "(?i)^\\s*\\w+\\s*\\w*\\s*$") String message) throws ClassNotFoundException {
+        String[] params = message.split("\\s");
+        switch (params.length) {
+            case 1:
+                rcm.processRoom(session.getCurrentRoom(), session.getGameState(), format(params[0]));
+                break;
+            case 2:
+                rcm.processRoom(session.getCurrentRoom(), session.getGameState(), format(params[0], params[1]));
+                break;
+        }
         return "";
     }
 
-    private String[] format(String command) {
-        String[] ret = command.split("\\s*");
-        for (String s :
-                ret) {
-            s.toUpperCase();
-        }
+    private String format(String command) {
+        String ret = command.toUpperCase();
         return ret;
     }
 
-//    ^\s*(go)\s+(room[1-5]{1})\s*$
+    private String format(String command, String parameter) throws ClassNotFoundException {
+        String[] parts = {command.toUpperCase(), parameter};
+        switch (parts[0]) {
+            case "REGISTER":
+                break;
+            case "GO":
+                parts[1] = "Room" + parameter.substring(4);
+                break;
+            default:
+                HashMap<String, String> methods = new HashMap<>();
+                for (Method m :
+                        Class.forName(session.getCurrentRoom()).getMethods()) {
+                    methods.put(m.getName().toUpperCase(), m.getName());
+                }
+                parts[0] = methods.get(parts[0]);
+                break;
+        }
+        return parts[0] + " " + parts[1];
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
+    //    ^\s*(go)\s+(room[1-5]{1})\s*$
 //    ^\s*(register)\s*\w+\s*$
 }
